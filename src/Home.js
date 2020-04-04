7f213cd27bccb2638e595633ba12425acac1c0cc
Diptarag <diptarag@gmail.com>
2020-04-05 01:21:56 +0530

    Home and sign in page

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,54 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { Container } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 import { Link as RouterLink } from 'react-router-dom';
 
+const useStyles = makeStyles(theme => ({
+  content: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  buttons: {
+    marginTop: theme.spacing(4),
+  }
+}));
+
 export default function Home() {
+  const classes = useStyles();
+
   return (
     <>
-      <h1>Welcome to simple sign in - sign up showcase app</h1>
-      <RouterLink to='/signin'>Sign In</RouterLink>
+      <CssBaseline />
+      <main>
+        <div className={classes.content}>
+          <Container maxWidth='sm'>
+            <Typography component='h1' variant='h2' align='center' color='textPrimary' gutterBottom>
+              Simple react forms
+            </Typography>
+            <Typography variant='h5' align='center' color='textSecondary' paragraph>
+              Simple react sign in & sign up forms using material ui, formik and yup
+            </Typography>
+            <div className={classes.buttons}>
+              <Grid container spacing={2} justify='center'>
+                <Grid item>
+                  <Button variant='contained' color='primary' component={RouterLink} to='/signin'>
+                    Sign In
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant='outlined' color='primary' component={RouterLink} to='/signup'>
+                    Sign Up
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+      </main>
     </>
   );
 }
--- src/SignIn.js
+++ src/SignIn.js
@@ -1,10 +1,136 @@
 import React from 'react';
+import { Formik, Field, Form } from 'formik';
+import * as Yup from 'yup';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Avatar from '@material-ui/core/Avatar';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 import { Link as RouterLink } from 'react-router-dom';
 
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
 export default function SignIn() {
+  const classes = useStyles();
+
   return (
-    <>
-      <h1>Welcome to Sign in page</h1>
-    </>
+    <Container component='main' maxWidth='xs'>
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component='h1' variant='h5'>
+          Sign in
+        </Typography>
+        <Formik
+          initialValues={{
+            email: '',
+            password: '',
+            rememberUser: false
+          }}
+          validationSchema={Yup.object({
+            email: Yup.string()
+              .email('Invalid email address')
+              .required('Required'),
+            password: Yup.string()
+              .required('No password provided.')
+          })}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 400);
+          }}>
+
+          <Form>
+            <Field name='email'>
+              {({ field, form, meta }) => (
+                <TextField
+                  variant='outlined'
+                  margin='normal'
+                  required
+                  fullWidth
+                  id='email'
+                  label='Email Address'
+                  name='email'
+                  autoComplete='email'
+                  autoFocus
+                  inputProps = {{...field}}
+                  error = {meta.touched && meta.error}
+                />
+              )}
+            </Field>
+            <Field name='password' type='password'>
+              {({ field, form, meta }) => (
+                <TextField
+                  variant='outlined'
+                  margin='normal'
+                  required
+                  fullWidth
+                  name='password'
+                  label='Password'
+                  type='password'
+                  id='password'
+                  autoComplete='current-password'
+                  inputProps = {{...field}}
+                  error = {meta.touched && meta.error}
+                />
+              )}
+            </Field>
+            <Field name='rememberUser' type='checkbox'>
+              {({ field }) => (
+                <FormControlLabel
+                  control={<Checkbox value='rememberUser' color='primary' inputProps = {{...field}} checked={field.value} />}
+                  label='Remember me'
+                />
+              )}
+            </Field>
+            <Button
+              type='submit'
+              fullWidth
+              variant='contained'
+              color='primary'
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href='#' variant='body2'>
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link component={RouterLink} to='/signup'>
+                    {'Don\'t have an account? Sign Up'}
+                  </Link>
+                </Grid>
+              </Grid>
+          </Form>
+        </Formik>
+      </div>
+    </Container>
   );
 }
